9fc96bd24d6b5b5d5f2f0ccc8ac5d9c2528cb961
abeyahc <abeyahc@gmail.com>
2024-05-31 14:58:09 -0700

    yes

---
+++ chicken game/Scripts/Healthbar.gd
@@ -0,0 +1,33 @@
+extends ProgressBar
+
+@onready var timer = $Timer
+@onready var damagebar = $Damagebar
+
+var health = 0
+
+func _set_health(new_health):
+	var prev_health = health
+	health = min(max_value, new_health)
+	value = health
+	
+	if health <= 0:
+		queue_free()
+		
+	if health < prev_health:
+		timer.start()
+	else:
+		damagebar.value = health # immediately fills up to health bar
+
+func init_health(_health):
+	health = _health
+	max_value = health
+	value = health
+	damagebar.max_value = health # both bars will be completely full
+	damagebar.value = health
+
+func _on_timer_timeout():
+	damagebar.value = health # damage bar catches up to health bar
+
+func _on_hurtbox_area_entered(area):
+	health -= 1
+
