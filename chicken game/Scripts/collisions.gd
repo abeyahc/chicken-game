fdbff877c6fee2615a454c955a700b5a8aced373
Jeijito <jj.ramgui@gmail.com>
2024-05-30 20:20:21 -0700

    All roof transparent

--- chicken game/Scripts/collisions.gd
+++ chicken game/Scripts/collisions.gd
@@ -6,31 +6,33 @@ extends TileMap
 var layer = 1
 var tile_coordinates = []
 var behind = false
+@onready var color_rect = $ColorRect
 
-func _ready():
-	# Assuming you are interested in layer 0. Change this if you need a different layer.
-	
-	# Get the used cells in the specified layer
+
+func _process(delta):
+	for i in range(collisions.get_layers_count()): #checks each layer 
+		layer_info(i + 1)
+
+func layer_info(layer):
 	for cell in collisions.get_used_cells(layer):
 		# Convert the cell coordinates to world position if needed
 		var world_position = collisions.map_to_local(cell)
 		tile_coordinates.append(world_position)
+		conditional_func(layer)
+	tile_coordinates.clear()
 
-
-func _process(delta):
+func conditional_func(layer):
 	# Get the player's position
 	var player_position = player.position
 	# Convert the player's position to the map (cell) position
 	var tile_below = collisions.local_to_map(player_position)
 	var world_tile_below = collisions.map_to_local(tile_below)
-	
 	behind = false
 	for coord in tile_coordinates:
 		if coord == world_tile_below:
 			behind = true
 			break
 	if behind:
-		modulate.a = 0.5  # Semi-transparent
+		set_layer_modulate(layer,Color(1, 1, 1, 0.5))  # Semi-transparent
 	else:
-		modulate.a = 1.0  # Fully opaque
-
+		set_layer_modulate(layer,Color(1, 1, 1, 1)) # Full visible
