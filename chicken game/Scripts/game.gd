01522c87f80a6bbe5fcbaf3b4a24bd93a0fbe08b
abeyahc <abeyahc@gmail.com>
2024-06-01 12:06:23 -0700

    yes

--- chicken game/Scripts/game.gd
+++ chicken game/Scripts/game.gd
@@ -1,35 +1,17 @@
 extends Node2D
 
-@onready var pause_menu = $Player/Camera2D/PauseMenu
 @onready var player = $Player
 @onready var fox = $fox
 const fox_scene = preload("res://Scenes/fox.tscn")
 @export var num_foxes = 2
 @onready var collisions = $collisions
+@onready var pause_menu = $CanvasLayer/PauseMenu
 
-var paused = false
 
 func _ready():
 	randomize()
 	spawn_foxes(num_foxes)
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_just_pressed("pause"):
-		pauseMenu()
-
-func pauseMenu():
-	if paused: # if resume putton is pressed return back to original screen
-		pause_menu.hide()
-		#Engine.time_scale = 1
-		get_tree().paused = false
-	else:
-		pause_menu.show()
-		#Engine.time_scale = 0
-		get_tree().paused = true
-		
-	paused = !paused
-
 	
 func spawn_foxes(count):
 	for i in range(count):
--- chicken game/Scripts/pause_menu.gd
+++ chicken game/Scripts/pause_menu.gd
@@ -1,8 +1,21 @@
 extends Control
 @onready var game = $"../../.."
 
+var is_paused:bool = false:
+	set(value):
+		is_paused = value
+		get_tree().paused = is_paused
+		visible = is_paused
+		
+func _ready() -> void:
+	is_paused = false
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		is_paused = !is_paused
+
 func _on_resume_pressed():
-	game.pauseMenu()
+	is_paused = false
 
 func _on_quit_pressed():
 	get_tree().quit()
