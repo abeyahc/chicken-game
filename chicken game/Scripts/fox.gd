21c2de000408354006cc19e0ac522480ef524b8e
Jeijito <118863740+Jeijito@users.noreply.github.com>
2024-06-05 14:46:35 -0700

    BASIC AI Finished

--- chicken game/Scripts/fox.gd
+++ chicken game/Scripts/fox.gd
@@ -1,80 +1,57 @@
 extends CharacterBody2D
 
-var state_machine
-var speed = 100
+var huh = false
+var speed = 85
 var acceleration = 7
 var player_chase = false
 @onready var animation = $AnimatedSprite2D
 @onready var timer = $Timer
+@onready var timer_nav = $Guidance/Timer
 
 var player = null
 @export var nav_agent: NavigationAgent2D
-
+var direction = Vector2.ZERO
 
 func _ready():
-	# connect animations
-	state_machine = $AnimatedSprite2D/AnimationTree.get("parameters/playback")
 	# connect timer's timeout signal to custom function
-	timer.connect("timeout", Callable(self, "_on_Timer_timeout"))
+	pass
 
 func _physics_process(delta):
-	var direction = Vector2.ZERO
-	
 	if player_chase:
+		animation.play("attack")
 		direction = nav_agent.get_next_path_position() - global_position
 		direction = direction.normalized()
 		velocity = velocity.lerp(direction * speed, acceleration*delta)
 		move_and_slide()
-	
-	#if player_chase:
-		# collide with tiles
-		#move_and_collide(Vector2(0,0)) 
 
-		# change position of enemy
-		#position += (player.position - position) / speed 
-		
-		#if (player.position.x - position.x) < 0:
-			#animation.flip_h = true
-		#else:
-			#animation.flip_h = false
-	#else:
-		#state_machine.travel("idle")
+		if velocity.x < 0:
+			animation.flip_h = true
+		else:
+			animation.flip_h = false
+	elif (player_chase == false) and (huh == false):
+		animation.play("idle")
 
-func recalc_path():
-	if player:
-		nav_agent.target_position = player.global_position
 
-func sleep():
-	state_machine.travel("sleep")
-func attack():
-	state_machine.travel("attack")
 
 func _on_detection_area_body_entered(body):
 	player = body
-	start_detect_sequence()
-
+	huh = true
+	animation.play("detect")
+	timer.start()
+	
 func _on_detection_area_body_exited(body):
 	player = null
 	player_chase = false
 	timer.stop()
-	velocity = Vector2.ZERO
-	
-func start_detect_sequence():
-	# Play the detect animation and start the timer
-	state_machine.travel("detect")
-	timer.start()  # Start the timer
 
-func _on_Timer_timeout():
-	# When the timer times out, start chasing the player
-	player_chase = true
-	recalc_path()
-	attack()  # Play the attack animation and start chasing
-
-func _on_path_completed():
-	player_chase = false
-	velocity = Vector2.ZERO  # Stop movement when the path is completed
-	sleep()  # Transition to sleep state if needed
-	
 
 func _on_timer_timeout_nav():
 	recalc_path()
+
+func _on_timer_timeout():
+	player_chase = true
+	huh = false
+	
+func recalc_path():
+	if player:
+		nav_agent.target_position = player.global_position
