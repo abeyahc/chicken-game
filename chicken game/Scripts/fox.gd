05edc119912861d286a54644b93a8df917097db6
Jeijito <jj.ramgui@gmail.com>
2024-06-15 02:31:49 -0700

    AI rework

--- chicken game/Scripts/fox.gd
+++ chicken game/Scripts/fox.gd
@@ -1,115 +1,94 @@
 extends CharacterBody2D
 
-var huh = false
-var speed = 85
+var speed = 90
 var acceleration = 7
-var player_chase = false
 @onready var animation = $AnimatedSprite2D
-@onready var timer = $Detection
+@onready var detection = $Detection
 @onready var update = $Guidance/Update
 @onready var ray_cast_2d = $RayCast2D
-var zone = false
-var detection_var = false
 @export var nav_agent: NavigationAgent2D
 var direction = Vector2.ZERO
 @onready var player = $"../Player"
-var escaping = false
-var wondering = false
-var idle_con = true
 @onready var idle_time = $Idle
-
-func _ready():
-	idle_time.start()
+var inside_area = false
+var chasing = false
+var spotted = false
+var wondering = false
+var relaxing = true
 
 func _physics_process(delta):
-	
 	var direction = (player.global_position - global_position).normalized()
 	var angle = direction.angle()
 	ray_cast_2d.rotation = angle
 	
-	if (ray_cast_2d.is_colliding() == false) and (player_chase == false) and (detection_var == false) and zone:
-		detection_var = true
-		detection()
-	
-	if escaping and global_position.distance_to(nav_agent.get_final_position()) < 5.0:
-		escaping = false
-		wondering = false
-		idle_con = true
-		idle_time.start()
-	
-	if (player_chase == true) or escaping:
-		animation.play("attack")
-		direction = nav_agent.get_next_path_position() - global_position
-		direction = direction.normalized()
-		velocity = velocity.lerp(direction * speed, acceleration*delta)
-		move_and_slide()
-
-		if velocity.x < 0:
-			animation.flip_h = true
+	if inside_area:
+		if chasing:
+			follow(delta)
 		else:
-			animation.flip_h = false
+			if ray_cast_2d.is_colliding():
+				idle(delta)
+			else:
+				if animation.get_animation() == "detect":
+					spotted = true
+				else:
+					animation.play("detect")
+					detection.start()
+	else:
+		if chasing:
+			spotted = false
+			if global_position.distance_to(nav_agent.get_final_position()) < 1.0:
+				chasing = false
+			else:
+				follow(delta)
+		else:
+			idle(delta)
 	
-	elif (player_chase == false) and (huh == false):
-		if idle_con and (wondering == false):
-			animation.play("idle")
-		elif (idle_con == false) and (wondering == false):
-			idle()
-		elif (idle_con == false) and wondering:
-			idle_condition(delta)
-		elif wondering and idle_con:
-			var wondering = false
-			var idle_con = true
-
 func _on_detection_area_body_entered(body):
-	zone = true
+	inside_area = true
 	
 func _on_detection_area_body_exited(body):
-	player_chase = false
-	zone = false
-	detection_var = false
-	escaping= true
-	timer.stop()
+	inside_area = false
 
 func _on_timer_timeout_nav():
 	recalc_path()
 
-func _on_timer_timeout():
-	player_chase = true
-	huh = false
-
-func recalc_path():
-	if zone and player_chase:
-		nav_agent.target_position = player.global_position
-
-func detection():
-	huh = true
-	animation.play("detect")
-	timer.start()
-
 func _on_idle_timeout():
-	idle_con = false
-	idle_time.stop()
+	if (wondering == false):
+		wondering = true
 
-func idle():
-	wondering = true
-	nav_agent.target_position = Vector2(randi_range(-601, 601), randi_range(-475, 600))
-	while(nav_agent.is_target_reachable() != true):
-		nav_agent.target_position = Vector2(randi_range(-601, 601), randi_range(-475, 600))
+func _on_detection_timeout():
+	chasing = true
 
-func idle_condition(delta):
+func follow(delta):
 	animation.play("attack")
 	direction = nav_agent.get_next_path_position() - global_position
 	direction = direction.normalized()
 	velocity = velocity.lerp(direction * speed, acceleration*delta)
 	move_and_slide()
-	
+
 	if velocity.x < 0:
 		animation.flip_h = true
 	else:
 		animation.flip_h = false
-	
-	if global_position.distance_to(nav_agent.get_final_position()) < 10.0:
-		wondering = false
-		idle_con = true
+
+func idle(delta):
+	if relaxing:
+		relaxing = false
 		idle_time.start()
-	
+		animation.play("idle")
+		find_loc()
+	if wondering:
+		follow(delta)
+		
+		if global_position.distance_to(nav_agent.get_final_position()) < 15.0:
+			wondering = false
+			relaxing = true
+
+func find_loc():
+	nav_agent.target_position = Vector2(randi_range(-601, 601), randi_range(-475, 600))
+	while(nav_agent.is_target_reachable() != true):
+		nav_agent.target_position = Vector2(randi_range(-601, 601), randi_range(-475, 600))
+
+func recalc_path():
+	if (inside_area and chasing) or spotted:
+		nav_agent.target_position = player.global_position
