83d78d9d3989f3b704acd3642c68ea9b8a558e5a
abeyahc <115514997+abeyahc@users.noreply.github.com>
2024-08-27 22:03:08 -0700

    yay

--- chicken game/Scripts/Wave.gd
+++ chicken game/Scripts/Wave.gd
@@ -3,8 +3,8 @@ extends RichTextLabel
 
 func _process(delta):
 	if Global.curr_wave == 1:
-		var text = str("You have survived ", str(Global.curr_wave), " wave\n", "Your highest is ", str(Global.high_wave))
-		self.text = text
+		var text = str("You Survived ", str(Global.curr_wave), " wave\n", "Your highest is ", str(Global.high_wave))
+		self.bbcode_text = "[center]" + text + "[/center]"
 	else:
-		var text = str("You have survived ", str(Global.curr_wave), " waves\n", "Your highest is ", str(Global.high_wave))
-		self.text = text
+		var text = str("You Survived ", str(Global.curr_wave), " waves\n", "Your highest is ", str(Global.high_wave))
+		self.bbcode_text = "[center]" + text + "[/center]"
